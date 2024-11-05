5d93eb30d69e41ae5c40bb066d7bc6755fb50727
halv1s <dev.ngtronghieu@gmail.com>
2024-11-05 13:45:02 +0700

    chore: basic routing

--- apps/IA03-User_Registration/src/app/page.tsx
+++ apps/IA03-User_Registration/src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="h-screen grid place-items-center">
-      <h1>Hieu Dep Trai</h1>
+    <div className="mt-24 flex flex-col justify-center items-center gap-4">
+      <Link href="/register">
+        <button>Register</button>
+      </Link>
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
