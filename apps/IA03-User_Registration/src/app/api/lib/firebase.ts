7ca4389b95781d652e5634fd1e2bfaddf3e77b38
halv1s <dev.ngtronghieu@gmail.com>
2024-11-05 15:57:04 +0700

    chore: use project id for db url

--- apps/IA03-User_Registration/src/app/api/lib/firebase.ts
+++ apps/IA03-User_Registration/src/app/api/lib/firebase.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: 'ia03-user-registration',
+    databaseURL: process.env.FIREBASE_PROJECT_ID,
   });
 }
 
