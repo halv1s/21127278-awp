3da030dfa4d83df12e805f0b918f33e5ae5c60de
halv1s <dev.ngtronghieu@gmail.com>
2024-11-05 16:55:14 +0700

    chore: continue debugging

--- apps/IA03-User_Registration/src/app/api/lib/firebase.ts
+++ apps/IA03-User_Registration/src/app/api/lib/firebase.ts
@@ -1,6 +1,11 @@
 import admin from 'firebase-admin';
 
-console.log('--> projectId', process.env.FIREBASE_PROJECT_ID);
+console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
+console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
+console.log(
+  'FIREBASE_PRIVATE_KEY:',
+  process.env.FIREBASE_PRIVATE_KEY ? 'Loaded' : 'Not loaded',
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
